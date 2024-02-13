1d5f61f578b326b3cf5b1122f28ba1f0db59e0c1
horstman26 <143755409+horstman26@users.noreply.github.com>
2024-02-13 13:47:33 -0800

    hw4
    create and handle post request for /orders, update the list with hardcoded values

--- hw3/public/javascripts/cheesecakejava.js
+++ hw3/public/javascripts/cheesecakejava.js
@@ -6,8 +6,55 @@
   /* this function is for the month dropdown to change the buttons current month */
   function monthSelect(monthNum) {
     document.getElementById("monthButton").innerHTML = monthNum;
-  }
+    
+    //send and handle a post request (for cakeData, in orders.js)
+    $.post('/orders', function(response) {
 
+      console.log(response);
+
+      //counter vars
+      let plainAmount= 0;
+      let cherryAmount = 0;
+      let chocAmount = 0;
+     
+      console.log("after var definition");
+
+      //loop through the response
+      for(var i = 0; i < response.data.length; i++){
+
+        console.log("made it into for loop");
+        
+        //var for the topping string from response
+        let stringData = response.data[i].topping;
+        console.log(stringData);
+
+        //check what type the topping is and increment the proper counter
+        if(stringData === "plain"){
+          console.log("plain = " + response.data[i].quantity);
+          plainAmount = plainAmount + response.data[i].quantity;
+        }
+        else if(stringData === "cherry"){
+          console.log("cherry = " + response.data[i].quantity);
+          cherryAmount = cherryAmount + response.data[i].quantity;
+        }
+        else if (stringData === "chocolate"){
+          console.log("choc = " + response.data[i].quantity);
+          chocAmount = chocAmount + response.data[i].quantity;
+        }
+        else {
+          console.log("error in if statement");
+        }
+      }
+
+      console.log("after for loop");
+
+      //replace the list elements (in index.html) with the new values
+      document.getElementById("listOne").innerHTML = cherryAmount + " cherry";
+      document.getElementById("listTwo").innerHTML = chocAmount + " chocolate";
+      document.getElementById("listThree").innerHTML = plainAmount + " plain";
+    
+    });
+  }
 
   /* this function is for the toppings radial buttons */
   function toppingType(flavorIn) {
--- hw3/routes/orders.js
+++ hw3/routes/orders.js
@@ -2,17 +2,20 @@
 var express = require('express');
 var router = express.Router();
 
-//json object
 var cakeData = [
-  { topping: 'cherry', quantity: '5' }, 
-  { topping: 'plain', quantity: '3' }, 
-  { topping: 'chocolate', quantity: '9' }
+  { topping: "cherry", quantity: 5 }, 
+  { topping: "plain", quantity: 3 }, 
+  { topping: "chocolate", quantity: 9 }
 ];
 
-
-/* GET users listing. */
+/*
 router.get('/', function(req, res, next) {
-  res.json({ error: null, data: cakeData} );
+  res.json({ error: null, data: cakeData});
+});
+*/
+
+router.post('/', function(req, res, next) {
+  res.json({ error: null, data: cakeData});
 });
 
 module.exports = router;
